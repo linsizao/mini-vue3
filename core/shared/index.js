94aff049ff1274569195478b568e660fd53726dd
linsizao <linsizao@163.com>
2023-02-07 17:18:51 +0800

    添加 shared 工具

---
+++ core/shared/index.js
@@ -0,0 +1,57 @@
+function looseCompareArrays (a, b) {
+  if (a.length !== b.length) return false
+  let equal = true
+  for (let i = 0; equal && i < a.length; i++) {
+    equal = looseEqual(a[i], b[i])
+  }
+  return equal
+}
+export const objectToString = Object.prototype.toString
+
+export const toTypeString = (value) => objectToString.call(value)
+
+const isDate = (val) => toTypeString(val) === '[object Date]'
+
+const isArray = Array.isArray
+
+const isObject = (val) => val !== null && typeof val === 'object'
+
+// 判断是否相等
+export function looseEqual (a, b) {
+  if (a === b) return true
+  let aValidType = isDate(a)
+  let bValidType = isDate(b)
+  if (aValidType || bValidType) {
+    return aValidType && bValidType ? a.getTime() === b.getTime() : false
+  }
+  aValidType = isArray(a)
+  bValidType = isArray(b)
+  if (aValidType || bValidType) {
+    return aValidType && bValidType ? looseCompareArrays(a, b) : false
+  }
+  aValidType = isObject(a)
+  bValidType = isObject(b)
+  if (aValidType || bValidType) {
+    /* istanbul ignore if: this if will probably never be called */
+    if (!aValidType || !bValidType) {
+      return false
+    }
+    const aKeysCount = Object.keys(a).length
+    const bKeysCount = Object.keys(b).length
+    if (aKeysCount !== bKeysCount) {
+      return false
+    }
+    for (const key in a) {
+      const aHasKey = a.hasOwnProperty(key)
+      const bHasKey = b.hasOwnProperty(key)
+      if (
+        (aHasKey && !bHasKey) ||
+        (!aHasKey && bHasKey) ||
+        !looseEqual(a[key], b[key])
+      ) {
+        return false
+      }
+    }
+  }
+  return String(a) === String(b)
+}
