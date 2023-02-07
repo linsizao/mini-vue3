5dc0ee3864752cece45420e22057f78313e3d85c
linsizao <linsizao@163.com>
2023-02-07 17:25:18 +0800

    diff 方法处理 children 变化

--- core/render/index.js
+++ core/render/index.js
@@ -1,3 +1,5 @@
+import { looseEqual } from '../shared/index.js'
+
 // 挂载节点（vNode -> dom）
 export function mountElement (vnode, container) {
   const { tag, props, children } = vnode
@@ -32,7 +34,6 @@ export function mountElement (vnode, container) {
 
 // TODO: 简单的 diff 算法
 export function diff (oldVnode, newVnode) {
-
   if (oldVnode.tag !== newVnode.tag) {
     // change tag
     oldVnode.el.replaceWith(document.createElement(newVnode.tag))
@@ -48,7 +49,7 @@ export function diff (oldVnode, newVnode) {
         const newVal = newProps[key]
         const oldVal = oldProps[key]
         if (newVal !== oldVal) {
-          oldVnode.el.setAttribute(key, newVal)
+          newVnode.el.setAttribute(key, newVal)
         }
       })
     }
@@ -57,12 +58,52 @@ export function diff (oldVnode, newVnode) {
       // 处理删除 prop
       Object.keys(oldProps).forEach(key => {
         if (!newProps[key]) {
-          oldVnode.el.removeAttribute(key)
+          newVnode.el.removeAttribute(key)
         }
       })
     }
   }
 
-  // TODO: change children
+  // change children(string/array)
+  // 1. newChildren: string; oldChildren: string/array
+  // 2. newChildren: array; oldChildren: string/array
+  const { children: oldChildren } = oldVnode
+  const { children: newChildren } = newVnode
+
+  if (!looseEqual(oldChildren, newChildren)) {
+    if (typeof newChildren === 'string' && newChildren !== oldChildren) {
+      newVnode.el.textContent = newChildren
+    }
+
+    if (Array.isArray(newChildren)) {
+      if (typeof oldChildren === 'string') {
+        newVnode.el.innerText = ''
+        mountElement(newVnode, newVnode.el)
+      } else if (Array.isArray(oldChildren)) {
+        const minLength = Math.min(newChildren.length, oldChildren.length)
+
+        // 最小长度内使用 diff
+        for (let i = 0; i < minLength; i++) {
+          const oldChildrenVnode = oldChildren[i]
+          const newChildrenVnode = newChildren[i]
+          diff(oldChildrenVnode, newChildrenVnode)
+        }
+
+        if (newChildren.length > minLength) {
+          // 创建节点
+          for (let i = minLength; i < newChildren.length; i++) {
+            const newChildrenVnode = newChildren[i]
+            mountElement(newChildrenVnode, newVnode.el)
+          }
+        } else if (newChildren < minLength) {
+          // 移除节点
+          for (let i = 0; i < oldChildren.length; i++) {
+            const oldChildrenVnode = oldChildren[i]
+            oldChildrenVnode.el.parent.removeChild(oldChildrenVnode.el)
+          }
+        }
+      }
+    }
+  }
 
 }
--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// // test
-
-// import { effectWatch } from "./reactivity/dep.js"
-// import { reactive } from "./reactivity/reactive.js"
-
-// const user = reactive({
-//   age: 10
-// })
-
-// let plus
-
-// effectWatch(() => {
-//   plus = user.age + 1
-//   console.log('---effectWatch---')
-//   console.log('user.age: ', user.age)
-//   console.log('plus: ', plus)
-// })
-
-// user.age = 20 // change value
-
-
 import App from './App.js'
 import { createdApp } from './core/index.js'
 
